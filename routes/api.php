d889f3ad1f22241e3c9d5e67dc9c3eaeaac0c973
OmarSalama17 <o.salama.abdallah@gmail.com>
2025-12-28 21:27:45 +0200

    create product get , post , put , delete , search getProduct by categoryId

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,5 +12,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::delete('/delete-account', [AuthController::class, 'deleteAccount']);
+
     Route::apiResource('categories', CategoryController::class);
+
+    Route::apiResource('products', ProductController::class);
+    Route::get('/products/search/{name}', [ProductController::class, 'search']);
+    Route::get('/products/category/{id}', [ProductController::class, 'getByCategoryId']);
 });
