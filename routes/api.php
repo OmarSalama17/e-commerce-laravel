8d9c4a5c69d3bf6c3d23410992feb3e34b87ec9d
OmarSalama17 <o.salama.abdallah@gmail.com>
2025-12-28 21:16:26 +0200

    create get , post , put , delete categories

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -10,5 +11,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::delete('/delete-account', [AuthController::class, 'deleteAccount']);
-
+    Route::apiResource('categories', CategoryController::class);
 });
